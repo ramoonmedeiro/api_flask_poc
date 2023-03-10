551ca453ee51a459250cf63f9b4a83029e28f393
Ramon Medeiro <r.medeiro10@gmail.com>
2023-03-10 11:18:12 -0300

    Adicionando método DELETE aos testes com requests

--- testes/teste-api.py
+++ testes/teste-api.py
@@ -12,21 +12,27 @@ def _post(url, d):
 def _put():
     pass
 
-def _delete():
-    pass
+def _delete(url):
+    r = requests.delete(url)
+    return json.loads(r.text)
 
 if __name__ == '__main__':
     
-    id_filme: int = 3
-    URL: str = f'http://localhost:8000/filmes/{id_filme}'
+    id_filme = 3
+    URL =f'http://localhost:8000/filmes/{id_filme}'
 
     # GET method
     #print(_get(url=URL))
 
     # POST Method
-    d = {
-        'titulo': 'Código Da Vinci',
-        'genero': 'ação',
-        'ano': 2003
-    }
-    print(_post(url=URL, d=d))
+    #d = {
+    #    'titulo': 'Código Da Vinci',
+    #    'genero': 'ação',
+    #    'ano': 2003
+    #}
+    #print(_post(url=URL, d=d))
+
+    # PUT method
+
+    # DELETE method
+    print(_delete(url=URL))
