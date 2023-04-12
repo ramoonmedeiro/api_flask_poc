5daeee54783ebe7d6f30b1eb063cbc3dbac8aac9
Ramon Medeiro <r.medeiro10@gmail.com>
2023-04-12 20:58:41 -0300

    feat: inserindo novamente o método get filmes

--- projeto1/main.py
+++ projeto1/main.py
@@ -22,63 +22,10 @@ filmes = {
 }
 
 # Método GET
+
 @app.get('/filmes')
 async def get_filmes():
     return filmes
-
-# Dando get para consumir o endpoint de um filme
-@app.get('/filmes/{filme_id}')
-async def get_filme(
-    filme_id: int = Path(
-    default=None, 
-    title='ID do filme',
-    description='Dever ser entre 1 e 2',
-    gt = 0, lt = 3 )
-    ):
-
-    try:    
-        filme = filmes[filme_id]
-        return filme
-    except:
-        raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, 
-                detail='Filme não encontrado'
-                )
-    
-# Método POST
-@app.post('/filmes', status_code=status.HTTP_201_CREATED)
-async def post_filme(filme: Filme):
-    next_id: int = len(filmes) + 1
-    filmes[next_id] = filme
-    del filme.id
-    return filme
-
-# Método PUT
-@app.put('/filmes/{filme_id}')
-async def put_filme(filme_id: int, filme: Filme):
-    if filme_id in filmes:
-        filmes[filme_id] = filme
-
-        return filme
-    else:
-        raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, 
-                detail=f'Não exite um filme com id={filme_id}'
-                )
-
-# Método DELETE
-@app.delete('/filmes/{filme_id}')
-async def delete_filme(filme_id: int):
-    if filme_id in filmes:
-        del filmes[filme_id]
-        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
-    else:
-        raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, 
-                detail=f'Não exite um filme com id={filme_id}'
-        )
-
-
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
